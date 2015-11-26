2458bd6ec548b9ac55d9463e197bbc2e8f2495e1
ddhunter13 <1310nero@mail.ru>
2015-11-26 17:55:01 +0300

    Maven

--- src/CD/CoderDecoderUI.java
+++ src/CD/CoderDecoderUI.java
@@ -46,13 +46,13 @@ public class CoderDecoderUI extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jScrollPane3 = new javax.swing.JScrollPane();
         outTextField = new javax.swing.JTextArea();
-        jPanel1 = new javax.swing.JPanel();
-        encodeLable = new javax.swing.JLabel();
-        encodeButton = new javax.swing.JButton();
-        decodeLabel = new javax.swing.JLabel();
-        decodeButton = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         inTextField = new javax.swing.JTextArea();
+        jPanel1 = new javax.swing.JPanel();
+        encodeLable = new javax.swing.JLabel();
+        decodeLabel = new javax.swing.JLabel();
+        encodeButton = new javax.swing.JButton();
+        decodeButton = new javax.swing.JButton();
         keyLabel = new javax.swing.JLabel();
         currentKeyLabel = new javax.swing.JLabel();
         codeMethodLabel = new javax.swing.JLabel();
@@ -146,11 +146,14 @@ public class CoderDecoderUI extends javax.swing.JFrame {
         outTextField.setRows(5);
         jScrollPane3.setViewportView(outTextField);
 
-        jPanel1.setMaximumSize(new java.awt.Dimension(300, 400));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        inTextField.setColumns(20);
+        inTextField.setRows(5);
+        inTextField.setMinimumSize(new java.awt.Dimension(1, 1));
+        jScrollPane2.setViewportView(inTextField);
 
         encodeLable.setText("Encode:");
-        jPanel1.add(encodeLable, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
+
+        decodeLabel.setText("Decode:");
 
         encodeButton.setText("►");
         encodeButton.addActionListener(new java.awt.event.ActionListener() {
@@ -158,10 +161,6 @@ public class CoderDecoderUI extends javax.swing.JFrame {
                 encodeButtonActionPerformed(evt);
             }
         });
-        jPanel1.add(encodeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 70, 43, 20));
-
-        decodeLabel.setText("Decode:");
-        jPanel1.add(decodeLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 110, -1, -1));
 
         decodeButton.setText("◄");
         decodeButton.addActionListener(new java.awt.event.ActionListener() {
@@ -169,12 +168,37 @@ public class CoderDecoderUI extends javax.swing.JFrame {
                 decodeButtonActionPerformed(evt);
             }
         });
-        jPanel1.add(decodeButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 110, -1, 20));
 
-        inTextField.setColumns(20);
-        inTextField.setRows(5);
-        inTextField.setMinimumSize(new java.awt.Dimension(1, 1));
-        jScrollPane2.setViewportView(inTextField);
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(decodeLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(decodeButton))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(encodeLable)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(encodeButton)))
+                .addContainerGap(25, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(encodeLable)
+                    .addComponent(encodeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(decodeLabel)
+                    .addComponent(decodeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(47, 47, 47))
+        );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -183,7 +207,7 @@ public class CoderDecoderUI extends javax.swing.JFrame {
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 261, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 244, Short.MAX_VALUE))
         );
@@ -192,7 +216,7 @@ public class CoderDecoderUI extends javax.swing.JFrame {
             .addComponent(jScrollPane3)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 182, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 5, Short.MAX_VALUE))
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
@@ -257,14 +281,14 @@ public class CoderDecoderUI extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(keyLabel)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(currentKeyLabel))
-                    .addGroup(layout.createSequentialGroup()
                         .addComponent(codeMethodLabel)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(selectedMethodLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(0, 0, Short.MAX_VALUE))
+                        .addComponent(selectedMethodLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(keyLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(currentKeyLabel)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -281,19 +305,11 @@ public class CoderDecoderUI extends javax.swing.JFrame {
                             .addComponent(keyLabel)
                             .addComponent(currentKeyLabel)))
                     .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(28, 28, 28))
+                .addGap(70, 70, 70))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void encodeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_encodeButtonActionPerformed
-        outTextField.setText(CoderDecoder.encode(inTextField.getText()));
-    }//GEN-LAST:event_encodeButtonActionPerformed
-
-    private void decodeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decodeButtonActionPerformed
-        inTextField.setText(CoderDecoder.decode(outTextField.getText()));
-    }//GEN-LAST:event_decodeButtonActionPerformed
 
     private void menuCodeMethodsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuCodeMethodsActionPerformed
         
@@ -327,6 +343,14 @@ public class CoderDecoderUI extends javax.swing.JFrame {
         dialogTextField.setText(KeyGenerator.generateKey());
     }//GEN-LAST:event_dilaogGenerateButtonActionPerformed
 
+    private void decodeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_decodeButtonActionPerformed
+        inTextField.setText(CoderDecoder.decode(outTextField.getText()));
+    }//GEN-LAST:event_decodeButtonActionPerformed
+
+    private void encodeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_encodeButtonActionPerformed
+        outTextField.setText(CoderDecoder.encode(inTextField.getText()));
+    }//GEN-LAST:event_encodeButtonActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -352,6 +376,9 @@ public class CoderDecoderUI extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(CoderDecoderUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         
         /* Create and display the form */
